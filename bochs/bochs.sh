21dc27305474846e64e71b1e27e186f6d56caa09
tedmosby <eikemail-com@yahoo.de>
2009-11-05 14:47:52 +0100

    ere

--- bochs/bochs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -
-
-CONFDIR="-q -f /home/ronny/Dokumente/Studi_Unterlagen/git/BS-stubsi/bochs/.bochsrc"
-if [ $# -eq 1 ]
-then
-	IMAGE="floppya:1_44=$1,status=inserted"
-fi
-
-bochs $CONFDIR $IMAGE
