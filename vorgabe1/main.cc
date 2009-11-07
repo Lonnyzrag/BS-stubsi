2937817f299ba35904872986db06448baaec76a6
Lonnyzrag <binary@quantentunnel.de>
2009-11-07 01:14:59 +0100

    test printing

--- vorgabe1/main.cc
+++ vorgabe1/main.cc
@@ -4,21 +4,21 @@
  CGA_Screen keyoutput;
  
 int main() {
-	int a,b = 0;
+	int a,b = 13;
 	int scrollcounter=2;
 	
     keyoutput.show(5,0, (char)65, (unsigned char) 15);
 	
-	for (int i; i <= 25; i++){
+	//for (int i; i <= 25; i++){
 		keyoutput.setpos(a,b);
 		keyoutput.print ("Eike, eIke, eiKe, eikE", 22,  15);
-		a++;
-		b=b+3;
-	}
-	//print (char* 'Dies ist ein Text!',(int) 18, (unsigned char) 15){
+		//a++;
+		//b=b+3;
+	//}
+	//keyoutput.print (char* 'Dies ist ein Text!',(int) 18, (unsigned char) 15){
    
    //Scrolltest:
-   keyoutput.scrollup(scrollcounter);
+   //keyoutput.scrollup(scrollcounter);
    
   return 0;
 }
